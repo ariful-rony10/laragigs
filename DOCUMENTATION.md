91c1b1f0179f294974590491589cd91cde1d02e1
Md. Ariful Islam <ariful.rony10@gmail.com>
2022-05-25 13:58:47 +0600

    Homepage done

---
+++ DOCUMENTATION.md
@@ -0,0 +1,9 @@
+# LARAGIGS
+
+- Routes in `routes` folder
+- Views in `resource/views`
+- Database 
+- Model in `app/Models`
+
+
+php artisan make:migration create_listings_table to create a table.
---
+++ resources/views/listings.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+
+    @include('partials._hero')
+    @include('partials._search')
+
+
+    @unless(count($listings) == 0)
+        @foreach ($listings as $listing)
+            <x-listing-card :listing="$listing" />
+        @endforeach
+    @else
+        <p>No Listings Found! - Try to add a lisgting!</p>
+    @endunless
+@endsection
