03f86f95f10d1f3117b0d961a691062d61fbe693
Md. Ariful Islam <ariful.rony10@gmail.com>
2022-05-25 14:18:27 +0600

    implement controller to handle the routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
 
@@ -15,17 +16,17 @@ use App\Models\Listing;
 */
 
 // All Listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 // Single Listings
-Route::get('/listings/{listing}', function(Listing $listing){
-    return view('listing', [
-        'listing' => $listing
-    ]); 
-    
-});
+Route::get('/listings/{listing}', [ListingController::class , 'show']);
+
+
+// Common Resource Routes:
+// index - Show All listings
+// show - show single listing
+// create - Show firm to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - delete  listing
